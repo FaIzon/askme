2134a7eabd263112b889d44a8eab917ae315d525
FaIzon <dimer90@gmail.com>
2017-02-20 21:30:22 +0700

    Add helpers and assets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,42 @@
-
 class UsersController < ApplicationController
+  def index
+    # # который создает модель, не записывая её в базу
+    # @users = [
+    #   User.new(
+    #     id: 1,
+    #     name: 'Vadim',
+    #     username: 'installero',
+    #     avatar_url: ''
+    #   ),
+    #   User.new(
+    #     id: 2,
+    #     name: 'Misha',
+    #     username: 'aristofun'
+    #   )
+    # ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
 
   def show
-    @hello = "Hello!"
+    # Болванка пользователя
+    @user = User.new(
+      name: 'Vadim',
+      username: 'installero'
+    )
+
+    # Болванка вопросов для пользователя
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('27.03.2016')),
+    ]
+
+    # Болванка для нового вопроса
+    @new_question = Question.new
+
   end
 end
