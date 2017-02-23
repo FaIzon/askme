6b7eb99780f2dbbf3385bd2989d2c05b6ddc64a3
FaIzon <dimer90@gmail.com>
2017-02-23 11:45:33 +0700

    Add quantity of questions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,33 @@ module ApplicationHelper
       asset_path 'cookie.jpg'
     end
   end
+
+  def declension_of_word(number, array_words, with_number = true)
+
+    if (number == nil || !number.is_a?(Numeric))
+      number = 0
+    end
+
+    prefix = ""
+    prefix = "#{number.to_s} " if with_number
+
+    remainder = number % 10
+
+    remainder100 = number % 100
+    if (remainder100 >= 11 && remainder100 <= 14)
+      return "#{prefix}#{array_words[2]}"
+    end
+
+    if (remainder == 1)
+      return "#{prefix}#{array_words[0]}"
+    end
+
+    if (remainder >= 2 && remainder <= 4)
+      return "#{prefix}#{array_words[1]}"
+    end
+
+    if (remainder >= 5 && remainder <= 9 || remainder == 0)
+      return "#{prefix}#{array_words[2]}"
+    end
+  end
 end
